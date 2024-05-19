1a38da712caa2bb1cad93e26c510c5ffd896cb26
Marco Antonio <marcoacoelho02@hotmail.com>
2024-05-19 19:02:01 -0300

    Deploy from CLI

--- obj/Release/net8.0/blazor1.AssemblyInfo.cs
+++ obj/Release/net8.0/blazor1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1799711055a459360d07446573722106b912e730")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a189b9de8f194d3ead4d9ccd0424defd6365d949")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
